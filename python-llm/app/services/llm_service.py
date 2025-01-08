14021bf1c1844ce4cd9738b728ee45be2d275a88
Gabriel Silva de Castro <gabrielcastrog2@gmail.com>
2025-01-07 23:03:47 -0300

    create: LLM Summarize python service

--- python-llm/app/services/llm_service.py
+++ python-llm/app/services/llm_service.py
@@ -4,16 +4,23 @@ from langchain_openai import OpenAI
 
 class LLMService:
     def __init__(self):
-        # Aqui assumimos que há uma variável de ambiente HF_TOKEN configurada.
         self.llm = OpenAI(
             temperature=0.5,
             top_p=0.7,
-            api_key=os.getenv("HF_TOKEN"),  # type: ignore
+            api_key=os.getenv("HF_TOKEN"), 
             base_url="https://api-inference.huggingface.co/models/Qwen/Qwen2.5-72B-Instruct/v1",
         )
 
-    def summarize_text(self, text: str) -> str:
-        prompt = f"{text}"
+    def summarize_text(self, text: str, lang: str) -> str:
+            supported_languages = {"pt": "Portuguese", "en": "English", "es": "Spanish"}
+            if lang not in supported_languages:
+                raise ValueError("Language not supported", 400)
 
-        response = self.llm.invoke(prompt)
-        return response
+            target_language = supported_languages[lang]
+            prompt = (
+                f"First, summarize the following text {text} "
+                f"Then, translate the summary into {target_language}:\n\n"
+            )
+
+            response = self.llm.invoke(prompt)
+            return response

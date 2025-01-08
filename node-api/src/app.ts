69d0602beacb9f32336feaab0cf912b11397e579
Gabriel Silva de Castro <gabrielcastrog2@gmail.com>
2025-01-08 01:37:30 -0300

    add: CORS for node api

--- node-api/src/app.ts
+++ node-api/src/app.ts
@@ -1,8 +1,10 @@
 import express, { Application } from 'express';
 import tasksRoutes from './routes/tasksRoutes';
+import cors from "cors";
 
 const app: Application = express();
 app.use(express.json());
+app.use(cors());
 app.use('/', tasksRoutes);
 
 export default app;

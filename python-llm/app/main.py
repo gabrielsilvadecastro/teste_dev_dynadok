2c416dfdc9c3bf3fdcb0fd5b9710147fb64b4731
Gabriel Silva de Castro <gabrielcastrog2@gmail.com>
2025-01-07 23:03:13 -0300

    create: python API summarize controllers

--- python-llm/app/main.py
+++ python-llm/app/main.py
@@ -8,16 +8,28 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from services.llm_service import LLMService
 
-app = FastAPI()
-llm_service = LLMService()
 
+title_project = "API Summarize Text"
+description_project = "Webservice destinado a realizar resumos de textos."
+contact_project={
+    "name": "Gabriel Silva de Castro",
+    "url": "https://github.com/gabrielsilvadecastro/teste_dev_dynadok",
+    "email": "gabrielcastrog2@gmail.com",
+}
+
+app = FastAPI(title=title_project, description=description_project, contact=contact_project)
+llm_service = LLMService()
 
 class TextData(BaseModel):
     text: str
+    lang: str
 
+@app.get("/")
+async def isrunning() -> dict:
+    return {"message": "API is running"}
 
 @app.post("/summarize")
 async def summarize(data: TextData):
-    text = data.text
-    llm_service.summarize_text(text)
-    return "OK"
+    llm_response = llm_service.summarize_text(text=data.text, lang=data.lang)
+    return {"summary": llm_response}
+

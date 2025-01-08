8403a3e148faadee2a47cb969b2a7993c477855b
Gabriel Silva de Castro <gabrielcastrog2@gmail.com>
2025-01-08 01:27:33 -0300

    update: index api node

--- node-api/src/index.ts
+++ node-api/src/index.ts
@@ -1,8 +1,11 @@
 import dotenv from "dotenv";
+import morgan from "morgan";
 dotenv.config();
 import app from "./app";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000; 
+
+app.use(morgan('dev'));
 
 app.listen(PORT, () => {
   console.log(`Node API rodando na porta ${PORT}`);

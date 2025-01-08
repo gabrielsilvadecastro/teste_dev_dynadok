24f620231bd57951e74c8e32b35851b91eab93eb
Gabriel Silva de Castro <gabrielcastrog2@gmail.com>
2025-01-08 01:27:09 -0300

    add: app api node

--- node-api/src/app.ts
+++ node-api/src/app.ts
@@ -3,8 +3,6 @@ import tasksRoutes from './routes/tasksRoutes';
 
 const app: Application = express();
 app.use(express.json());
-
-// Rotas
-app.use('/tasks', tasksRoutes);
+app.use('/', tasksRoutes);
 
 export default app;

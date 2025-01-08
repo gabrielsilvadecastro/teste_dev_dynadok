78a8cfc9f2089413012afe2ec8aa9780d87c7c9e
Gabriel Silva de Castro <gabrielcastrog2@gmail.com>
2025-01-08 01:29:54 -0300

    update: tasks routers for api node

--- node-api/src/routes/tasksRoutes.ts
+++ node-api/src/routes/tasksRoutes.ts
@@ -1,42 +1,18 @@
-import { Router, Request, Response } from "express";
-import { TasksRepository } from "../repositories/tasksRepository";
+import { Router } from "express";
+import { createTask, getTaskById, removeTask, listTasks} from "../controllers/tasksController";
 
 const router = Router();
-const tasksRepository = new TasksRepository();
 
-// POST: Cria uma tarefa e solicita resumo ao serviço Python
-router.post("/", async (req: Request, res: Response) => {
-  try {
-    const { text } = req.body;
-    if (!text) {
-      return res.status(400).json({ error: 'Campo "text" é obrigatório.' });
-    }
-
-    // Cria a "tarefa"
-    const task = tasksRepository.createTask(text);
-
-    // Deve solicitar o resumo do texto ao serviço Python
-    const summary = "Resumo da tarefa";
-
-    // Atualiza a tarefa com o resumo
-    tasksRepository.updateTask(task.id, summary);
-
-    return res.status(201).json({
-      message: "Tarefa criada com sucesso!",
-      task: tasksRepository.getTaskById(task.id),
-    });
-  } catch (error) {
-    console.error("Erro ao criar tarefa:", error);
-    return res
-      .status(500)
-      .json({ error: "Ocorreu um erro ao criar a tarefa." });
-  }
-});
-
-// GET: Lista todas as tarefas
 router.get("/", (req, res) => {
-  const tasks = tasksRepository.getAllTasks();
-  return res.json(tasks);
-});
+    res.send({"message":"API is Running"});
+  });  
+
+router.post("/tasks", createTask);
+
+router.get("/tasks/:id", getTaskById);
+
+router.delete("/tasks/:id", removeTask);
+
+router.get("/tasks", listTasks);
 
 export default router;
